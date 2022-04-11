7826ee5150d74f59ba0bef86ca6cd091c7221348
Lucas ALLOIN <lucas.alloin915@gmail.com>
2022-04-11 16:28:23 +0200

    add: build

--- dist/chains/index.js
+++ dist/chains/index.js
@@ -1,10 +1,22 @@
 var _a;
 /* Anima Supported Chains */
 var ETH = "ETH";
+var BSC = "BSC";
+var AURORA = "AURORA";
+var EVMChains = {
+    "1": ETH,
+    "58": BSC,
+    "1313161554": AURORA,
+};
 var AnimaChains = (_a = {},
     _a[ETH] = true,
+    _a[BSC] = true,
+    _a[AURORA] = true,
     _a);
 function IsSupported(chain) {
     return AnimaChains[chain];
 }
-export default { ETH: ETH, IsSupported: IsSupported };
+function GetChainIdByChainName(chainId) {
+    return EVMChains[chainId];
+}
+export default { ETH: ETH, IsSupported: IsSupported, GetChainIdByChainName: GetChainIdByChainName };
