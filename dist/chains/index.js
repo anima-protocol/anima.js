573309d3665d73dc27e117a0698c19a44d3603d5
Lucas ALLOIN <lucas.alloin915@gmail.com>
2022-04-27 11:08:17 +0200

    add: build lib

--- dist/chains/index.js
+++ dist/chains/index.js
@@ -6,22 +6,27 @@ var BSC_NAME = "BSC";
 var BSC_ID = "56";
 var AURORA_NAME = "AURORA";
 var AURORA_ID = "1313161554";
+var FANTOM_NAME = "FANTOM";
+var FANTOM_ID = "250";
 var EVMChainsId = (_a = {},
     _a[ETH_NAME] = ETH_ID,
     _a[BSC_NAME] = BSC_ID,
     _a[AURORA_NAME] = AURORA_ID,
+    _a[FANTOM_NAME] = FANTOM_ID,
     _a);
 var EVMChainsName = (_b = {},
     _b[ETH_ID] = ETH_NAME,
     _b[BSC_ID] = BSC_NAME,
     _b[AURORA_ID] = AURORA_NAME,
+    _b[FANTOM_ID] = FANTOM_NAME,
     _b);
 var AnimaChains = (_c = {},
     _c[ETH_NAME] = true,
     _c[BSC_NAME] = true,
     _c[AURORA_NAME] = true,
+    _c[FANTOM_NAME] = true,
     _c);
-var EVMChain = [ETH_NAME, BSC_NAME, AURORA_NAME];
+var EVMChain = [ETH_NAME, BSC_NAME, AURORA_NAME, FANTOM_NAME];
 function IsSupported(chain) {
     return AnimaChains[chain];
 }
