732b6828b31d6a21586dd2cf23a644638b72e823
Lucas ALLOIN <lucas.alloin915@gmail.com>
2022-04-27 10:39:34 +0200

    add: fantom compat

--- chains/index.ts
+++ chains/index.ts
@@ -8,25 +8,31 @@ const BSC_ID = "56";
 const AURORA_NAME = "AURORA"
 const AURORA_ID = "1313161554";
 
+const FANTOM_NAME = "FANTOM"
+const FANTOM_ID = "250";
+
 const EVMChainsId: { [key: string]: string} = {
   [ETH_NAME]: ETH_ID,
   [BSC_NAME]: BSC_ID,
   [AURORA_NAME]: AURORA_ID,
+  [FANTOM_NAME]: FANTOM_ID,
 }
 
 const EVMChainsName: { [key: string]: string} = {
   [ETH_ID]: ETH_NAME,
   [BSC_ID]: BSC_NAME,
   [AURORA_ID]: AURORA_NAME,
+  [FANTOM_ID]: FANTOM_NAME,
 }
 
 const AnimaChains: { [key: string]: boolean } = {
   [ETH_NAME]: true,
   [BSC_NAME]: true,
   [AURORA_NAME]: true,
+  [FANTOM_NAME]: true,
 };
 
-const EVMChain = [ETH_NAME, BSC_NAME, AURORA_NAME]
+const EVMChain = [ETH_NAME, BSC_NAME, AURORA_NAME, FANTOM_NAME]
 
 function IsSupported(chain: string): boolean {
   return AnimaChains[chain];
