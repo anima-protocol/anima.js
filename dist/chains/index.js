d51f28ab58b8681621a3c80e98e52e29beedb4cc
Lucas ALLOIN <lucas@synaps.io>
2022-07-25 11:16:16 +0200

    :sparkles: polygon build support

--- dist/chains/index.js
+++ dist/chains/index.js
@@ -10,12 +10,15 @@ var FANTOM_NAME = "FANTOM";
 var FANTOM_ID = "250";
 var MOONBEAM_NAME = "MOONBEAM";
 var MOONBEAM_ID = "1284";
+var POLYGON_NAME = "POLYGON";
+var POLYGON_ID = "137";
 var EVMChainsId = (_a = {},
     _a[ETH_NAME] = ETH_ID,
     _a[BSC_NAME] = BSC_ID,
     _a[AURORA_NAME] = AURORA_ID,
     _a[FANTOM_NAME] = FANTOM_ID,
     _a[MOONBEAM_NAME] = MOONBEAM_ID,
+    _a[POLYGON_NAME] = POLYGON_ID,
     _a);
 var EVMChainsName = (_b = {},
     _b[ETH_ID] = ETH_NAME,
@@ -23,6 +26,7 @@ var EVMChainsName = (_b = {},
     _b[AURORA_ID] = AURORA_NAME,
     _b[FANTOM_ID] = FANTOM_NAME,
     _b[MOONBEAM_ID] = MOONBEAM_NAME,
+    _b[POLYGON_ID] = POLYGON_NAME,
     _b);
 var AnimaChains = (_c = {},
     _c[ETH_NAME] = true,
@@ -30,8 +34,9 @@ var AnimaChains = (_c = {},
     _c[AURORA_NAME] = true,
     _c[FANTOM_NAME] = true,
     _c[MOONBEAM_NAME] = true,
+    _c[POLYGON_NAME] = true,
     _c);
-var EVMChain = [ETH_NAME, BSC_NAME, AURORA_NAME, FANTOM_NAME, MOONBEAM_NAME];
+var EVMChain = [ETH_NAME, BSC_NAME, AURORA_NAME, FANTOM_NAME, MOONBEAM_NAME, POLYGON_NAME];
 function IsSupported(chain) {
     return AnimaChains[chain];
 }
