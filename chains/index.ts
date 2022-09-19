fc245d3183576a53a368979fa30942c08022c2e0
Lucas ALLOIN <lucas@synaps.io>
2022-09-19 15:49:09 +0200

    :twisted_rightwards_arrows: merge all evm compat update

--- chains/index.ts
+++ chains/index.ts
@@ -1,10 +1,12 @@
 const EVM_NAME = "EVM";
+const ELROND_NAME = "ELROND";
 
 const AnimaChains: { [key: string]: boolean } = {
   [EVM_NAME]: true,
+  [ELROND_NAME]: true,
 };
 
-const EVMChain = [EVM_NAME]
+const EVMChain = [EVM_NAME, ELROND_NAME]
 
 function IsSupported(chain: string): boolean {
   return AnimaChains[chain];
