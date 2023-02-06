1c3e90b41ad36336dbca12c34ed68c23557f19a3
Lucas ALLOIN <lucas@synaps.io>
2023-02-06 17:42:30 +0100

    :sparkles: update signing message template

--- challenges/encryption_request.ts
+++ challenges/encryption_request.ts
@@ -1,9 +1,11 @@
 
-export function GetEncryptionRequest(nonce: string): string {
-    return `ANIMA ENCRYPTION REQUEST.
+export function GetEncryptionRequest(nonce: string, public_address: string): string {
+    return `Sign this message to decrypt your document and gain access to the information it contains. This confirms your ownership of the wallet and allows you to interact with the document's data
 
-/!\ BE AWARE TO BE ON TRUSTED WEBSITE.
-        
-NONCE: ${nonce}
+Wallet address:
+${public_address}
+
+Nonce:
+${nonce}
 `
 }
--- dist/challenges/encryption_request.js
+++ dist/challenges/encryption_request.js
@@ -1,8 +1,10 @@
-export function GetEncryptionRequest(nonce) {
-    return `ANIMA ENCRYPTION REQUEST.
+export function GetEncryptionRequest(nonce, public_address) {
+    return `Sign this message to decrypt your document and gain access to the information it contains. This confirms your ownership of the wallet and allows you to interact with the document's data
 
-/!\ BE AWARE TO BE ON TRUSTED WEBSITE.
-        
-NONCE: ${nonce}
+Wallet address:
+${public_address}
+
+Nonce:
+${nonce}
 `;
 }
