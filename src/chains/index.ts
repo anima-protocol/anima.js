83b8cf3974c03ed8ca9b3f08a6c8d345184a5909
Lucas ALLOIN <lucas@synaps.io>
2023-04-17 16:11:04 +0200

    :sparkles: add cosmos & starknet chain support

--- src/chains/index.ts
+++ src/chains/index.ts
@@ -1,9 +1,13 @@
 const EVM_NAME = "EVM";
 const ELROND_NAME = "ELROND";
+const COSMOS_NAME = "COSMOS";
+const STARKNET_NAME = "STARKNET";
 
 const AnimaChains: { [key: string]: boolean } = {
   [EVM_NAME]: true,
   [ELROND_NAME]: true,
+  [COSMOS_NAME]: true,
+  [STARKNET_NAME]: true,
 };
 
 export const EVMChain = [EVM_NAME, ELROND_NAME];
