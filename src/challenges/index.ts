314739b25b3d9c66d7ad525381135c576ac3b150
Raphael Dalmon <raphael@synaps.io>
2023-05-15 10:25:16 +0200

    🔧 library has no side effects

--- src/challenges/index.ts
+++ src/challenges/index.ts
@@ -1,8 +1,10 @@
+import { CreateAnimaRequest } from "./anima_create_request";
 import { GetEncryptionRequest } from "./encryption_request";
 import { GetIssuingRequest } from "./issuing_request";
 import { GetSharingRequest } from "./sharing_request";
 
 export default {
+  CreateAnimaRequest,
   GetEncryptionRequest,
   GetIssuingRequest,
   GetSharingRequest,
